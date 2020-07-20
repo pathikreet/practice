59b1df1e95a1c5bf5d3651d1b5eefc035ad83d6b
Oracle Public Cloud User <opc@dockervm.sub07201103220.vcnpractice.oraclevcn.com>
2020-07-20 13:26:54 +0000

    Initializing Respository

---
+++ hello-world/practice/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package practice;
+
+/**
+ * @author 801876
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from inside the OCI Docker VM");
+		System.out.println("This small program in running on Docker Conatiner at "+System.currentTimeMillis());
+	}
+
+}
+
